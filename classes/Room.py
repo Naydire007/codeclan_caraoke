9832059c09f9b930f18e3b77ea57ddffefdc51f3
Stephanie <stephania.l.e@gmail.com>
2021-06-28 03:46:16 +0100

    Extensions part 1

--- classes/Room.py
+++ classes/Room.py
@@ -8,15 +8,7 @@ class Room:
         self.room_name = room_name
         self.guest_list = []
         self.songs_list = []
-        # self.room1 = room1
-        # self.room1 = []
-        # self.song_list_room1 = []
-        # self.room2 = room2
-        # self.song_list_room2 = []
-        # self.room3 = room3
-        # self.song_list_room3 = []
-        # self.VIP = VIP
-        # self.song_list_VIP = []
+
        
         self.room_limit = 6
         self.VIP_limit = 14
